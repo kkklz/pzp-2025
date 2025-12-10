1425b861e72d507630b4dc855afd56268c6a31c4
Kwiasek <kwiasekkwiasek@gmail.com>
2025-12-10 17:43:24 +0100

    feat(app): 5 Created teams store

--- app/types/database.types.ts
+++ app/types/database.types.ts
@@ -208,19 +208,19 @@ export type Database = {
       teams: {
         Row: {
           created_at: string | null
-          created_by: string | null
+          created_by: string
           id: string
           name: string
         }
         Insert: {
           created_at?: string | null
-          created_by?: string | null
+          created_by?: string
           id?: string
           name: string
         }
         Update: {
           created_at?: string | null
-          created_by?: string | null
+          created_by?: string
           id?: string
           name?: string
         }
